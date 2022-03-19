dc2d507e1f9a5dff93c4f61a15b69cca6a95df6c
theanuragkumar <theanuragkumarofficial@gmail.com>
2022-03-19 18:41:53 +0530

    Updates

---
+++ precache-manifest.a4f0ffeeeb0a1b798028593036b5d438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf47883bc15f0ca57692bd4ebd6c32a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a36549fcd61d41e32257",
+    "url": "/portfolio/static/css/main.8d57d951.chunk.css"
+  },
+  {
+    "revision": "e97bb980b0b01b4a2971",
+    "url": "/portfolio/static/js/2.b1a6c79f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b1a6c79f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36549fcd61d41e32257",
+    "url": "/portfolio/static/js/main.724fcba8.chunk.js"
+  },
+  {
+    "revision": "1a5ad2fa4121c24d322d",
+    "url": "/portfolio/static/js/runtime-main.3fc07cb0.js"
+  }
+]);
